d1a8cdeb6a7d7c5369b6f145328f9f328e888fe2
Nikos Filippakis <aesmade@gmail.com>
2018-06-22 17:17:52 +0300

    Setup script for dotfiles
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# Run with sh
+cd $(dirname "$0")
+
+# Create symbolic links to bin/everything
+mkdir "$HOME/bin"
+find bin/ -type f | while read i; do
+    ln -s "$(readlink -f $i)" "$HOME/$i"
+done
+
+# Create symbolic links to everything in .dirs
+find . -mindepth 1 -type d -iname '.*' | while read i; do
+    if [ "$i" != "./.git" ]; then
+        find "$i" -type d | while read l; do
+            mkdir "$HOME/$l"
+        done
+        find "$i" -type f | while read l; do
+            ln -s "$(readlink -f $l)" "$HOME/$l"
+        done
+    fi
+done
+
+# Create symbolic links to all .files
+find . -type f -iname '.*' | while read i; do
+    if [ "$i" != "./.gitconfig" ] && [ "$i" != "./.gitignore" ]; then
+        ln -s "$(readlink -f $i)" "$HOME/$i"
+    fi
+done
